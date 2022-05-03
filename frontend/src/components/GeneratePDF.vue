a96a3cde261d59b1415297936ee0b399b83d4b40
millerserhii <millerserhii@gmail.com>
2022-05-03 15:03:26 +0200

    server deployment

---
+++ frontend/src/components/GeneratePDF.vue
@@ -0,0 +1,38 @@
+<template>
+   <div>
+     <VueHtml2pdf
+        :show-layout="false"
+        :float-layout="true"
+        :enable-download="true"
+        :preview-modal="true"
+        :paginate-elements-by-height="1400"
+        filename="myPDF"
+        :pdf-quality="2"
+        :manual-pagination="false"
+        pdf-format="a4"
+        pdf-orientation="landscape"
+        pdf-content-width="800px"
+        ref="html2Pdf"
+    >
+        <section slot="pdf-content">
+            <Badge-form/>
+        </section>
+    </VueHtml2pdf>
+   </div>
+</template>
+
+<script>
+import VueHtml2pdf from 'vue-html2pdf'
+import BadgeForm from '@/components/BadgeForm'
+ 
+export default { 
+    components: {
+        VueHtml2pdf
+    },
+    methods: { 
+        generateReport () {
+            this.$refs.html2Pdf.generatePdf()
+        }
+    },
+}
+</script>
